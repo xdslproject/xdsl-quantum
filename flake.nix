320c1a21599d5b11758292576e1516d1544c7ee2
Alex Rice <alexrice999@hotmail.co.uk>
2026-03-03 10:02:28 +0000

    installation: (nix) add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "xDSL quantum devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+          {
+            devShells.default = with pkgs; mkShell {
+              LD_LIBRARY_PATH = lib.makeLibraryPath [ stdenv.cc.cc.lib zlib ];
+              buildInputs = [
+                uv
+                llvmPackages_21.mlir
+              ];
+            };
+          }
+    );
+}
