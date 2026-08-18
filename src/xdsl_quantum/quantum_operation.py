28908a9b36cf2ba2bfd874783a54a257a392fd76
Alex Rice <alexrice999@hotmail.co.uk>
2026-08-18 14:23:19 +0100

    core: add QuantumOperationAttribute

---
+++ src/xdsl_quantum/quantum_operation.py
@@ -0,0 +1,72 @@
+"""Quantum gates and operations are likely to be shared between different dialects.
+Rather than creating a duplicate operation in each dialect, we encode each quantum
+operation as an attribute. The `QuantumOperationAttribute` generalises over gate
+operations and measurements, and specifies the number of classical inputs and outputs
+the operation has.
+
+We consider all operations to be non destructive, i.e. they have the same number of
+input and output qubits, but ultimately the dialect can decide how these are used.
+
+The use of these attributes has two advantages:
+- Static properties of common gates and subprocedures can be encoded once on the
+attribute, rather than being redefined for every dialect these gates are used in.
+- Certain transformations may be able to act in an operation independent way, for
+instance translating from the 'qssa' to 'qref' dialect does not need any knowledge of
+which quantum operations are being performed.
+"""
+
+from abc import ABC, abstractmethod
+
+from xdsl.ir import ParametrizedAttribute, TypeAttribute
+
+
+class QuantumOperationAttribute(ParametrizedAttribute, ABC):
+    """
+    Base attribute for all quantum operations.
+    """
+
+    @property
+    @abstractmethod
+    def classical_inputs(self) -> tuple[TypeAttribute, ...]: ...
+
+    @property
+    @abstractmethod
+    def classical_results(self) -> tuple[TypeAttribute, ...]: ...
+
+    @property
+    @abstractmethod
+    def num_qubits(self) -> int: ...
+
+
+class GateAttribute(QuantumOperationAttribute, ABC):
+    """
+    A quantum gate is an operation with no classical inputs or outputs
+    """
+
+    @property
+    def classical_inputs(self) -> tuple[()]:
+        return ()
+
+    @property
+    def classical_results(self) -> tuple[()]:
+        return ()
+
+
+class SingleQubitGateAttribute(GateAttribute, ABC):
+    """
+    A quantum gate on a single qubit.
+    """
+
+    @property
+    def num_qubits(self) -> int:
+        return 1
+
+
+class TwoQubitGateAttribute(GateAttribute, ABC):
+    """
+    A quantum gate on two qubits.
+    """
+
+    @property
+    def num_qubits(self) -> int:
+        return 2
