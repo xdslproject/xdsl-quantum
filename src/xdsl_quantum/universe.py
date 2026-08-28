24510986107e1698129a3b421125c8ed025e5c7d
Alex Rice <alexrice999@hotmail.co.uk>
2026-08-28 14:07:13 +0100

    misc: remove resolved todo

--- src/xdsl_quantum/universe.py
+++ src/xdsl_quantum/universe.py
@@ -1,7 +1,7 @@
 from xdsl.universe import Universe
 
-from xdsl_quantum.dialects import get_all_dialects  # TODO: Change this
-from xdsl_quantum.transforms import get_all_passes  # TODO: Change this
+from xdsl_quantum.dialects import get_all_dialects
+from xdsl_quantum.transforms import get_all_passes
 
 UNIVERSE = Universe(
     all_dialects=get_all_dialects(),
